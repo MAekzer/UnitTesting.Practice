8260367903282fee344608726a60ad6a5bc40613
MAekzer <gma312@yandex.ru>
2022-11-06 16:21:19 +0300

    Добавьте файлы проекта.

---
+++ Calcualtor.Tests/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Calculator;
+
+namespace Calcualtor.Tests
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        Calc calculator = new();
+
+        [Test]
+        public void DivisionMustReturnCorrectNumber()
+        {
+            Assert.That(calculator.Division(15, 3) == 5);
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(2, 0));
+        }
+
+        [Test]
+        public void AdditionMustReturnCorrectNumber()
+        {
+            Assert.That(calculator.Addition(15, 3) == 18);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectNumber()
+        {
+            Assert.That(calculator.Subtraction(15, 3) == 12);
+        }
+
+        [Test]
+        public void MultiplicationnMustReturnCorrectNumber()
+        {
+            Assert.That(calculator.Multiplication(15, 3) == 45);
+        }
+    }
+}
